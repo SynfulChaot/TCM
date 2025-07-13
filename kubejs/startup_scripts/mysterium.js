8fff207679761ce8b905deca1baf76547bbc40e1
SynfulChaot <synfulchaot@gmail.com>
2025-07-13 11:11:00 -0800

    Initial commit for 1.21.1
    ... finally!

--- kubejs/startup_scripts/mysterium.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-// Add Edelwood Lava Bucket as Furnace fuel
-ItemEvents.modification( event => {
-    event.modify( 'forbidden_arcanus:edelwood_lava_bucket' , item => {
-        item.burnTime = 20000
-    })
-})
